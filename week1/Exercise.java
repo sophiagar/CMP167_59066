06994a2a0802dee847dd1f7a4ab989cf6eda5fd4
sophiagar <78580956+sophiagar@users.noreply.github.com>
2021-02-04 23:50:19 -0500

    Update Exercise.java

--- week1/Exercise.java
+++ week1/Exercise.java
@@ -1,5 +1,31 @@
-//Write a program that welcomes the user and ask their name
-//then ask the user for 3 numbers and print out the product and the sum of those numbers
-public class Exercise {
+  import java.util.Scanner;
 
+/**
+ * 
+ * @author Sophia Garcia
+ * created 2/4/21
+ */
+
+public class Exercise {
+	public static void main(String[] args) {
+	System.out.print("Welcome! ");
+	Scanner sc = new Scanner(System.in); 
+	System.out.println("Enter your first name: ");
+	
+	Scanner scan = new Scanner(System.in);
+	System.out.print("Enter first number: ");
+	int firstNum = sc.nextInt();
+	System.out.print("Enter second number: ");
+	int secondNum = sc.nextInt();
+	System.out.print("Enter third number: ");
+	int thirdNum = sc.nextInt();
+		
+	int product = firstNum * secondNum * thirdNum;
+	int sum = firstNum + secondNum + thirdNum;
+		
+	System.out.println("Product: " + product);
+				
+	System.out.println("Sum: " + sum);
+		 
+	}
 }
